ed1a7460c2bd970593d12603d371663aa8177c2e
turchimatteo <turchi12@gmail.com>
2022-02-18 14:14:32 +0100

    add edit and index

---
+++ resources/views/cars/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('main_content')
+    <section>
+        <div class="container">
+            <div class="card">
+                <img src="{{ $card->img }}" alt="">
+                <div class="card-content">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
