17b34ea548b4306b41be1bc7d83a61c69465b077
turchimatteo <turchi12@gmail.com>
2022-02-18 14:38:35 +0100

    add show

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -6,7 +6,19 @@
             <div class="card">
                 <img src="{{ $card->img }}" alt="">
                 <div class="card-content">
-                    
+                    <h3 class="card-text">{{ $car->marca }}</h3>
+                    <h4 class="card-text">{{ $car->modello }}</h4>
+                    <h4 class="card-text">{{ $car->cilindrata }}</h4>
+                    <h4 class="card-text">{{ $car->porte }}</h4>
+                    <div><a class="btn btn-primary" href="{{ route('cars.edit', ['car' => $car->id]) }}">Modifica</a></div>
+                    <div>
+                        <form action="{{ route('cars.destroy', ['car' => $car->id]) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+  
+                            <button class="btn btn-danger" onclick="return confirm('Sei sicuro di voler cancellare?')">Cancella</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
