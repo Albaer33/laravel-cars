d6540d98d01b22209706014d812796ceb3bbb14b
Albaer33 <alisi3396@gmail.com>
2022-02-18 11:56:23 +0100

    scaffolding

---
+++ config/cars.php
@@ -0,0 +1,39 @@
+<?php
+    return[
+        [
+            "marca"=>"FIAT",
+            "modello"=>"Panda",
+            "cilindrata"=>" 999cc",
+            "porte"=>"5",
+            "img"=>"https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640_jpg/public/serie_auto_galleria/2011/12/fiat_panda_ant_0.png?itok=1MgNMqsq"
+        ],
+        [
+            "marca"=>"Lamborghini",
+            "modello"=>"Urus",
+            "cilindrata"=>" 3996cc",
+            "porte"=>"5",
+            "img"=>"https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640_jpg/public/serie_auto_galleria/2018/04/lamborghini_urus_post_0.png?itok=Vz9TkmJN"
+        ],
+        [
+            "marca"=>"Maserati",
+            "modello"=>"MC20",
+            "cilindrata"=>"3000cc",
+            "porte"=>"3",
+            "img"=>"https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640_jpg/public/serie_auto_galleria/2020/09/maserati_mc20_ant.png?itok=pyh1IU9x"
+        ],
+        [
+            "marca"=>"Maserati",
+            "modello"=>"MC20",
+            "cilindrata"=>"3000cc",
+            "porte"=>"3",
+            "img"=>"https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640_jpg/public/serie_auto_galleria/2020/09/maserati_mc20_ant.png?itok=pyh1IU9x"
+        ],
+        [
+            "marca"=>"Tesla",
+            "modello"=>"Model X",
+            "cilindrata"=>"Null",
+            "porte"=>"6",
+            "img"=>"https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640_jpg/public/serie_auto_galleria/2016/05/tesla_model-x_ant_0.png?itok=wSnNb9qW"
+        ],
+    ];
+?>
