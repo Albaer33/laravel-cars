92bc7b2652a9586c2aaa19004af2ae2de2530f32
painteyes <andreantoniocchipinti@gmail.com>
2022-03-01 16:50:54 +0100

    commit change

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Alber\Desktop\Boolean\Esercizi\18.02\laravel-cars\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Alber\Desktop\Boolean\Esercizi\18.02\laravel-cars\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\andre\Classe 48\laravel-cars\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\andre\Classe 48\laravel-cars\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
